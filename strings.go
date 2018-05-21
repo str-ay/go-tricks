b440c3c03e5bae4399eddee371d05a4d414e3665
Aleksandr Streltsov <jness.pro@ya.ru>
2018-05-21 12:46:59 +0300

    code rebase

--- strings.go
+++ strings.go
@@ -1,9 +1,27 @@
 package tricks
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
+
+//JoinTrimmed makes Join with trim all blank spaces like [ ], \t, "\n"
+func JoinTrimmed(sep string, str ...string) string {
+	trimmed := make([]string, 0, len(str))
+	for _, str := range str {
+		s := strings.TrimSpace(str)
+		if s == "" {
+			continue
+		}
+
+		trimmed = append(trimmed, s)
+	}
+
+	return strings.Join(trimmed, sep)
+}
 
 type buffer struct {
 	r         []byte
@@ -59,4 +77,39 @@ func CamelCaseToLowerWithDelimiter(s string, delimiter byte) string {
 		b.write(m)
 	}
 	return string(b.r)
+}
+
+// RepeatSeparated repeats string with delimiter
+// RepeatSeparated("?", ",", 4) -> "?,?,?,?"
+func RepeatSeparated(s string, sep string, number int) string {
+	if number < 1 {
+		return ""
+	}
+
+	return s + strings.Repeat(sep+s, number-1)
+}
+
+//MD5Hash generates MD5 string
+func MD5Hash(text string) (string, error) {
+	md5 := md5.New()
+	if _, err := md5.Write([]byte(text)); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(md5.Sum(nil)), nil
+}
+
+// UniqueStrings ...
+func UniqueStrings(strs []string) []string {
+	stringsMap := make(map[string]bool)
+	for _, str := range strs {
+		stringsMap[str] = true
+	}
+
+	results := make([]string, 0, len(stringsMap))
+	for k := range stringsMap {
+		results = append(results, k)
+	}
+
+	return results
 }
--- tricks.go
+++ tricks.go
@@ -2,26 +2,8 @@ package tricks
 
 import (
 	"bytes"
-	"crypto/md5"
-	"encoding/hex"
 	"strconv"
-	"strings"
 )
-
-//JoinTrimmed makes Join with trim all blank spaces like [ ], \t, "\n"
-func JoinTrimmed(sep string, str ...string) string {
-	trimmed := make([]string, 0, len(str))
-	for _, str := range str {
-		s := strings.TrimSpace(str)
-		if s == "" {
-			continue
-		}
-
-		trimmed = append(trimmed, s)
-	}
-
-	return strings.Join(trimmed, sep)
-}
 
 // QuestionMarks is useful for dynamic SQL:
 // in(" + QuestionMarks(len(ids)) + ") -> in(?,?,?,?,?)
@@ -58,39 +40,4 @@ func QueryArguments(number, start int) string {
 	}
 
 	return result.String()
-}
-
-// RepeatSeparated repeats string with delimiter
-// RepeatSeparated("?", ",", 4) -> "?,?,?,?"
-func RepeatSeparated(s string, sep string, number int) string {
-	if number < 1 {
-		return ""
-	}
-
-	return s + strings.Repeat(sep+s, number-1)
-}
-
-//MD5Hash generates MD5 string
-func MD5Hash(text string) (string, error) {
-	md5 := md5.New()
-	if _, err := md5.Write([]byte(text)); err != nil {
-		return "", err
-	}
-
-	return hex.EncodeToString(md5.Sum(nil)), nil
-}
-
-// UniqueStrings ...
-func UniqueStrings(strs []string) []string {
-	stringsMap := make(map[string]bool)
-	for _, str := range strs {
-		stringsMap[str] = true
-	}
-
-	results := make([]string, 0, len(stringsMap))
-	for k := range stringsMap {
-		results = append(results, k)
-	}
-
-	return results
 }
